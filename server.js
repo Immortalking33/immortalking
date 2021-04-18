7f0dbdc821ac5c01152dfd1204d597bf82414fe6
Palle Raabjerg <github@raabjerg.mm.st>
2021-04-18 13:45:37 +0200

    Move sync code into separate handler.

--- server.js
+++ server.js
@@ -18,10 +18,12 @@ const getUserCount = (roomId) => {
 const setRoomState = (roomId, state) => {
   const room = io.sockets.adapter.rooms[roomId];
   room.state = state;
+  console.log("Set room state:", room.state);
 }
 
 const getRoomState = (roomId) => {
   const room = io.sockets.adapter.rooms[roomId];
+  console.log("Get room state:", room.state);
   return room.state;
 }
 
@@ -30,10 +32,12 @@ const recalcRoomTime = (roomId, videoProgress) => {
   if (!room.time) room.time = {};
   room.time.date = new Date();
   room.time.progress = videoProgress;
+  console.log("Recalced room time:", room.time.date, room.time.progress);
 }
 
 const getVideoProgress = (roomId) => {
   const room = io.sockets.adapter.rooms[roomId];
+  console.log("Get room progress:", room.time);
   if (!room.time) return null;
 
   const additionalProgress =
@@ -49,24 +53,23 @@ const io = socketIO(server);
 
 io.on('connection', socket => {
   const roomId = socket.handshake.query['room'] || socket.id;
-  let videoProgress = parseInt(socket.handshake.query['videoProgress']);
+  // let videoProgress;
 
-  console.log('Received connection try', { roomId, videoProgress });
+  console.log('Received connection try', { roomId });
 
   socket.on('disconnect', () => console.log(`Client from room ${roomId} disconnected`));
 
   socket.join(roomId, () => {
+    // If no progress is registered, we just start at zero and paused
+    let isFirst = false;
     if (getVideoProgress(roomId) === null) {
-      recalcRoomTime(roomId, videoProgress);
+      console.log("Progress was null");
+      recalcRoomTime(roomId, 0);
+      setRoomState(roomId, RoomStates.PAUSED);
+      isFirst = true;
     }
 
-    const userCount = getUserCount(roomId);
-    videoProgress = getVideoProgress(roomId);
-    setRoomState(roomId, RoomStates.PAUSED);
-    const roomState = getRoomState(roomId);
-
-    socket.emit('join', roomId, roomState, videoProgress, userCount);
-    socket.to(roomId).emit('update', socket.id, roomState, videoProgress, userCount);
+    socket.emit('join', roomId, isFirst);
   });
 
   socket.on('update', (videoState, videoProgress) => {
@@ -76,12 +79,21 @@ io.on('connection', socket => {
     recalcRoomTime(roomId, videoProgress);
 
     const roomState = getRoomState(roomId);
-    videoProgress = getVideoProgress(roomId);
-    socket.to(roomId).emit('update', socket.id, roomState, videoProgress, userCount);
+    const updatedVideoProgress = getVideoProgress(roomId);
+    socket.to(roomId).emit('update', socket.id, roomState, updatedVideoProgress, userCount);
   });
 
   socket.on('chat', (nick, message) => {
     console.log('Received Chat from ', socket.id);
     io.in(roomId).emit('chat', socket.id, nick, message);
+  });
+
+  // Emit current progress and state to single watcher on resync request
+  socket.on('resync', () => {
+    const userCount = getUserCount(roomId);
+    const roomState = getRoomState(roomId);
+    const videoProgress = getVideoProgress(roomId);
+
+    socket.emit('update', socket.id, roomState, videoProgress, userCount);
   });
 });
